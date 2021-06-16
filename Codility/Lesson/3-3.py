3e9112b294f7e241cdca4b6629d2f81ec975eb75
nu1997 <jiu.1997.park@gmail.com>
2021-06-17 02:44:59 +0900

    [Codility] Lesson 3-2까지

--- Codility/Lesson/3-3.py
+++ Codility/Lesson/3-3.py
@@ -1,10 +1,16 @@
-def solution(A):
-    # write your code in Python 3.6
+# DP
+# 수를 저장해놓고 한칸이 넘어갔을 때 뭐가 되는지 생각?
 
+def solution(A):
     l = len(A)
-    ans = 987654321
-    for i in range(1, l):
-        temp = abs(sum(A[:i]) - sum(A[i:]))
-        if temp < ans:
-            ans = temp
-    return ans
+    memo = [0] * l
+    memo[1] = A[0] - sum(A[1:])
+    res = abs(memo[1])
+
+    for i in range(1, l - 1):
+        memo[i + 1] = memo[i] + (A[i] * 2)
+        print(memo[i+1], memo[i])
+        if abs(memo[i + 1]) < res:
+            res = abs(memo[i + 1])
+
+    return res

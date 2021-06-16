38a787f02b545bf89d2d8c92b7209bde35cd8b28
nu1997 <jiu.1997.park@gmail.com>
2021-06-17 02:46:47 +0900

    remove confidential

--- SSAFY/BaekJoon 특강/13706_제곱근.py
+++
@@ -1,5 +0,0 @@
-N = int(input())
-for i in range(1, N//2):
-    if N % i == 0:
-        if i * i == N:
-            print(i)
--- SSAFY/BaekJoon 특강/3260_두수의덧셈.py
+++
@@ -1,5 +0,0 @@
-T = int(input())
-for tc in range(1, T+1):
-    a, b = map(int, input().split())
-    res = a + b
-    print('#%d' % tc, res)

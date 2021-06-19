1dff07b734155e0d3d77b8653d9dd21d1e052708
nu1997 <jiu.1997.park@gmail.com>
2021-06-19 23:40:49 +0900

    [BOJ] 2884.알람시계

---
+++ BOJ/2884.py
@@ -0,0 +1,13 @@
+x = int(input())
+y = int(input())
+if x > 0:
+    if y > 0:
+        res = 1
+    else:
+        res = 4
+else:
+    if y > 0:
+        res = 2
+    else:
+        res = 3
+print(res)

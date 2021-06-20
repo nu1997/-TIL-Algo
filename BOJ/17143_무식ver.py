954b88eb021272eeddf60fc699476079ff36a49d
nu1997 <jiu.1997.park@gmail.com>
2021-06-20 23:56:53 +0900

    [BOJ] if문 ~ for문

--- BOJ/17143_무식ver.py
+++
@@ -1,8 +0,0 @@
-drc = [[-1, 0], [1, 0], [0, 1], [0, -1]]
-R, C, M = map(int, input().split())
-# arr = [[0]*C for _ in range(R)]
-shark = []
-
-for _ in range(M):
-    r, c, s, d, z = map(int, input().split())
-    shark = [r, c, s, d, z]

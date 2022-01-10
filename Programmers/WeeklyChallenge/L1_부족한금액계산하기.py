f1e73a058da3f3984ac3d2507f4ce31ea3b7db1f
ji woo park <jiu.1997.park@gmail.com>
2022-01-11 00:34:36 +0900

    [Programmers] Python / L1 / WeeklyChallege / ON-GOING

---
+++ Programmers/WeeklyChallenge/L1_부족한금액계산하기.py
@@ -0,0 +1,8 @@
+def solution(price, money, count):
+    total = 0
+    for i in range(1, count+1):
+        total += price * i
+    if money >= total:
+        return 0
+    else:
+        return abs(money - total)

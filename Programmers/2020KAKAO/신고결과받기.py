fadd939f5ca704b5b8041a62e4ac9ec16aa0620b
ji woo park <jiu.1997.park@gmail.com>
2022-01-16 01:01:52 +0900

    [Programmers] Python / L1 / 2020KAKAO / ON-GOING

---
+++ Programmers/2020KAKAO/신고결과받기.py
@@ -0,0 +1,15 @@
+def solution(id_list, report, k):
+    user_dict = {user: [[], 0] for user in id_list}
+    user_dict_new = {user: 0 for user in id_list}
+    for case in report:
+        a, b = case.split()
+        if a not in user_dict[b][0]:
+            user_dict[b][1] += 1
+            user_dict[b][0].append(a)
+    user_list = list(user_dict.values())
+    for log in user_list:
+        if log[1] >= 2:
+            for user in log[0]:
+                user_dict_new[user] += 1
+    answer = list(user_dict_new.values())
+    return answer

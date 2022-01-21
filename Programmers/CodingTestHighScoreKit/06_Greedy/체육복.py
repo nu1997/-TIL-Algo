b6a7e38d3d203e16eb6a86146705d43caa43b0f4
ji woo park <jiu.1997.park@gmail.com>
2022-01-21 23:44:46 +0900

    [Programmers] Python / L1 / Greedy / ON-GOING

--- Programmers/CodingTestHighScoreKit/06_Greedy/체육복.py
+++ Programmers/CodingTestHighScoreKit/06_Greedy/체육복.py
@@ -1,3 +1,32 @@
 def solution(n, lost, reserve):
     answer = 0
+    check = [1] * n
+    for student in lost:
+        check[student-1] -= 1
+    for student in reserve:
+        check[student-1] += 1
+    for i in range(n):
+        if check[i] == 0:
+            try:
+                if i > 0:
+                    if check[i-1] > 1:
+                        check[i-1] -= 1
+                        check[i] += 1
+                    elif check[i+1] > 1:
+                        check[i+1] -= 1
+                        check[i] += 1
+                    else:
+                        continue
+                else:
+                    if check[i+1] > 1:
+                        check[i+1] -= 1
+                        check[i] += 1
+                    else:
+                        continue
+                    
+            except:
+                pass
+    for num in check:
+        if num >= 1:
+            answer += 1
     return answer

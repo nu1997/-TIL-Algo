0892b1d0467455fa1aad1dc635ab209bb8ee389b
nu1997 <jiu___@naver.com>
2022-05-12 17:59:45 +0900

    [BOJ] Python / Level / 17478

--- Python/BOJ/Level/08_기본수학1/06_2775.py
+++ Python/BOJ/Level/08_기본수학1/06_2775.py
@@ -7,6 +7,8 @@
 아파트에 비어있는 집은 없고 모든 거주민들이 이 계약 조건을 지키고 왔다고 가정했을 때, 주어지는 양의 정수 k와 n에 대해 k층에 n호에는 몇 명이 살고 있는지 출력하라. 단, 아파트에는 0층부터 있고 각층에는 1호부터 있으며, 0층의 i호에는 i명이 산다.
 '''
 
+# Ver.1 시간초과
+
 def solution(a, k):
     if a == 1:
         return k*(k+1)//2
@@ -21,6 +23,4 @@ for tc in range(T):
     n = int(input())
     print(solution(k,n))
 
-    # for i in range(k):
-    #     n = n*(n+1)//2
-    # print(n)
+

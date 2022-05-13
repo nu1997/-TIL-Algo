5c06a232176353a1dffbc4198d7341b8045b8a1f
nu1997 <jiu___@naver.com>
2022-05-14 00:32:10 +0900

    [BOJ] Python / Silver-5 / 2751 / (study) merge sort

--- Python/BOJ/Silver/5/2751.py
+++ Python/BOJ/Silver/5/2751.py
@@ -10,56 +10,54 @@
 '''
 import sys
 
-# 병합 정렬 merge sort
-'''
-def merge(left, right):
-    result = []
-
-    # left, right 원소가 남아있는 경우
-    while len(left) > 0 and len(right) > 0:
-        # left, right 두 서브 리스트의 첫 원소들을 비교하여 작은 것부터 result에 추가
-        if left[0] <= right[0]:
-            result.append(left.pop(0))
-        else:
-            result.append(right.pop(0))
-    
-    if len(left) > 0: # 왼쪽 리스트에 원소가 남아있는 경우
-        result.extend(left)
-    
-    if len(right) > 0: # 오른쪽 리스트에 원소가 남아있는 경우
-        result.extend(right)
-    return result
-
-def merge_sort(m):
-    if len(m) <= 1:
-        return m
-
-    # divide
-    mid = len(m) // 2
-    left = m[:mid]
-    right = m[mid:]
-    
-    # recursion
-    left = merge_sort(left)
-    right = merge_sort(right)
-
-    # conquer
-    return merge(left, right)
-'''
-
 # 힙 정렬 heap sort
-
 # 퀵 정렬 quick sort
-
-
+# 카운트 정렬 count sort
 # 내장함수 sort()
-# arr.sort()
 
+# 병합 정렬 merge sort
+def merge_sort1(arr):
+    def merge(left, right):
+        result = []
 
+        # left, right 원소가 남아있는 경우
+        while len(left) and len(right):
+            # left, right 두 서브 리스트의 첫 원소들을 비교하여 작은 것부터 result에 추가
+            if left[0] <= right[0]:
+                result.append(left.pop(0))
+            else:
+                result.append(right.pop(0))
+        
+        if len(left) > 0: # 왼쪽 리스트에 원소가 남아있는 경우
+            result.extend(left)
+        
+        if len(right) > 0: # 오른쪽 리스트에 원소가 남아있는 경우
+            result.extend(right)
+        return result
 
-def merge_sort(arr):
+    def sort(m):
+        if len(m) <= 1:
+            return m
+
+        # divide
+        mid = len(m) // 2
+        left = m[:mid]
+        right = m[mid:]
+        
+        # recursion
+        left = merge_sort(left)
+        right = merge_sort(right)
+
+        # conquer
+        return merge(left, right)
+    return sort(arr)
+
+# 병합 정렬 merge sort - optimized
+def merge_sort2(arr):
     def sort(left, right):
+        print('sort function:', left, right)
         if right - left < 2:
+            print('right - left < 2: ', left, right)
             return
         mid = (left + right) // 2
         sort(left, mid)
@@ -67,26 +65,32 @@ def merge_sort(arr):
         merge(left, mid, right)
 
     def merge(left, mid, right):
+        print('merge function: ', left, mid, right)
         temp = []
         l, h = left, mid
 
         while l < mid and h < right:
             if arr[l] < arr[h]:
+                print(arr[l], '<', arr[h])
                 temp.append(arr[l])
                 l += 1
             else:
+                print(arr[l], '>=', arr[h])
                 temp.append(arr[h])
                 h += 1
 
         while l < mid:
+            print(l, '<', mid)
             temp.append(arr[l])
             l += 1
         while h < right:
+            print(h, '<', right)
             temp.append(arr[h])
             h += 1
 
         for i in range(left, right):
             arr[i] = temp[i - left]
+            print(arr)
 
     return sort(0, len(arr))
 
@@ -95,5 +99,6 @@ nums = []
 for _ in range(N):
     nums.append(int(sys.stdin.readline()))
 # nums.sort()
-merge_sort(nums)
-print(*nums, sep='\n')
+print(nums)
+merge_sort2(nums)
+# print(*nums, sep='\n')

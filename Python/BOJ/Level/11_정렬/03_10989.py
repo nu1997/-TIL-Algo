b539b8d9c5d617ef605c4b6d85517a423f87b632
nu1997 <jiu___@naver.com>
2022-05-31 23:34:25 +0900

    [BOJ] Python / Level / 정렬 / 10989

---
+++ Python/BOJ/Level/11_정렬/03_10989.py
@@ -0,0 +1,24 @@
+# 카운팅 정렬
+N = int(input())
+
+nums = []
+for i in range(N):
+    nums.append(int(input()))
+limit = max(nums) + 1
+
+count = [0] * limit
+# check = [0] * limit
+ans = [0] * N
+
+for num in nums:
+    # check[num] += 1
+    while num < limit:
+        count[num] += 1
+        num += 1
+
+for i in range(N-1, -1, -1): #987654321
+    count[nums[i]] -= 1
+    ans[count[nums[i]]] = nums[i]
+
+for num in ans:
+    print(num)

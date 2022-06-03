ea119cb04df558f3162133d5ae78072bae2148ee
nu1997 <jiwooapark@gmail.com>
2022-06-04 00:10:54 +0900

    [BOJ] Python / Level / 브루트 포스 / 7568

--- Python/BOJ/Level/10_BruteForce/03_7568.py
+++ Python/BOJ/Level/10_BruteForce/03_7568.py
@@ -1,4 +1,14 @@
 N = int(input())
+
+lst = []
 for i in range(N):
-    x, y = map(int, input().split())
-    
+    lst.append(input().split())
+
+ans = []
+for i in range(N):
+    grade = 1
+    for j in range(N):
+        if int(lst[i][0]) < int(lst[j][0]) and int(lst[i][1]) < int(lst[j][1]):
+            grade += 1
+    ans.append(grade)
+print(*ans)

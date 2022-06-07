3dc2d2ccea211c35ae1e5c6ee21d3c1619f293b4
nu1997 <jiwooapark@gmail.com>
2022-06-07 09:04:11 +0900

    [BOJ] Python / Level / 브루트 포스 / 1018

--- Python/BOJ/Level/10_BruteForce/04_1018.py
+++ Python/BOJ/Level/10_BruteForce/04_1018.py
@@ -2,15 +2,31 @@
 N, M = map(int, input().split())
 board = [list(input()) for _ in range(N)]
 
-# print(board)
-
-cnt = 0
-for i in range(M - 8 + 1):
-    for j in range(N - 8 + 1):
+ans = 987654321
+for i in range(N - 8 + 1):
+    for j in range(M - 8 + 1):
+        cnt_1, cnt_2 = 0, 0
         start = board[i][j]
-        print(start, i, j, board[i][j:j+8])
-        temp = start
+        # print(start, i, j, board[i][j:j+8])
+        other = 'B' if start == 'W' else 'W'
         for p in range(8):
             for q in range(8):
-                if temp != board[i+p][j+q]:
-                    temp = board[i+p][j+q]:
+                if (p % 2) ^ (q % 2):
+                    if board[i+p][j+q] != start:
+                        cnt_1 += 1
+                else:
+                    if board[i+p][j+q] != other:
+                        cnt_1 += 1
+        for r in range(8):
+            for s in range(8):
+                if (r % 2) ^ (s % 2):
+                    if board[i+r][j+s] != other:
+                        cnt_2 += 1
+                else:
+                    if board[i+r][j+s] != start:
+                        cnt_2 += 1
+        cnt = cnt_1 if cnt_1 < cnt_2 else cnt_2
+        if cnt < ans:
+            # print(i, j)
+            ans = cnt
+print(ans)

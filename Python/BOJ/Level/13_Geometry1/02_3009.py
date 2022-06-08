bc8a52bcd1fb5fe7e6bc9516131a1b1fa4a2c164
nu1997 <jiwooapark@gmail.com>
2022-06-09 03:24:29 +0900

    [BOJ] Python / Level / SGeometry / 13009 +

--- Python/BOJ/Level/13_Geometry1/02_3009.py
+++ Python/BOJ/Level/13_Geometry1/02_3009.py
@@ -3,6 +3,7 @@ pair = []
 for _ in range(3):
     pair.append(tuple(map(int, input().split())))
 
+# 그리디 - 넣었다 뺏다
 x, y = [], []
 for coor in pair:
     if coor[0] in x:
@@ -14,3 +15,11 @@ for coor in pair:
     else:
         y.append(coor[1])
 print(*x, *y)
+
+'''
+# XOR 연산 - 왜인지 메모리, 시간은 똑같다. 
+x = pair[0][0] ^ pair[1][0] ^ pair[2][0]
+y = pair[0][1] ^ pair[1][1] ^ pair[2][1]
+
+print(x, y)
+'''

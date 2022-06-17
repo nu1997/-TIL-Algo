ea19555b3ae9f80475f8340faca491a64de7fe3e
nu1997 <jiwooapark@gmail.com>
2022-06-17 20:56:26 +0900

    [BOJ] Python / Level / BackTracking / 15629 undone

--- Python/BOJ/Level/15_Backtracking/01_15649.py
+++ Python/BOJ/Level/15_Backtracking/01_15649.py
@@ -2,11 +2,4 @@
 N, M = map(int, input().split())
 # 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
 
-'''
-from itertools import combinations
-
-lst = combinations([x for x in range(1, N+1)], M)
-for el in lst:
-    print(el)
-'''
-
+# itertools combination?

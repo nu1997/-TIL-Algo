1a6340f4775f601dc92a799bdf1811f92650bfe3
nu1997 <jiwooapark@gmail.com>
2022-07-09 01:51:57 +0900

    [HackerRank] Python / Basic Data Types / Lists CORRECT

--- Python/HackerRank/2_BasicDataTypes/05_Lists.py
+++ Python/HackerRank/2_BasicDataTypes/05_Lists.py
@@ -1,3 +1,4 @@
+'''
 if __name__ == '__main__':
     N = int(input())
     arr = []
@@ -48,3 +49,25 @@ if __name__ == '__main__':
             arr.pop()
         elif el[0] == "reverse":
             arr.sort(reverse=True)
+'''
+
+N = int(input())
+lst = []
+arr = []
+for _ in range(N):
+    lst.append(input().split())
+for el in lst:
+    if el[0] == "insert":
+        arr.insert(int(el[1]), int(el[2]))
+    elif el[0] == "print":
+        print(arr)
+    elif el[0] == "remove":
+        arr.remove(int(el[1]))
+    elif el[0] == "append":
+        arr.append(int(el[1]))
+    elif el[0] == "sort":
+        arr.sort()
+    elif el[0] == "pop":
+        arr.pop()
+    elif el[0] == "reverse":
+        arr = arr[::-1]

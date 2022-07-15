a53f5a87b5f84e42d138b2b93c2ecd9277d0ad22
nu1997 <jiwooapark@gmail.com>
2022-07-15 23:12:10 +0900

    [CodeTree] C++ / Basic / 출력 / 기본 출력 /  다양하게 출력

---
+++ CPP/CodeTree/Basic/01_Print/1_Basic_Print/exam.cpp
@@ -0,0 +1,8 @@
+// 다양하게 출력
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Total days in Year" << endl << 365 << endl << "Circumference rate" << endl << "3.1415926535";
+  return 0;
+}
